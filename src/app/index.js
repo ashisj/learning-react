75fc27d0444967febd1e413e400b8d94e5da4fd4
Ashis Jena <ashis.jena96@gmail.com>
2019-07-15 20:13:39 +0530

    adding multiple components

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
 class App extends React.Component {
 
     render() {
@@ -8,7 +11,14 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <h1>Hello!</h1>
+                        <Header/>
+                    </div>
+                </div>
+                <p>yooo</p>
+                <br/>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
                     </div>
                 </div>
             </div>
