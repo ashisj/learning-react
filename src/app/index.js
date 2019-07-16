274eb941630e794547a1f7542c122b8cd88303b6
Ashis Jena <ashis.jena96@gmail.com>
2019-07-16 15:19:27 +0530

    different lifecycle hooks in reactJS

--- src/app/index.js
+++ src/app/index.js
@@ -8,18 +8,39 @@ class App extends React.Component {
     constructor(){
         super();
         this.state = {
-            homeLink: "Home"
+            homeLink: "Home",
+            homeMounted: true
         };
     }
     onGreet(){
         alert("Hello!");
     }
+
     onChangeLinkName(newName){
         this.setState({
             homeLink : newName
         });
     }
+
+    onChangeHomeMounted(){
+        this.setState({
+            homeMounted: !this.state.homeMounted
+        });
+    }
+
     render() {
+        let homeCmp = ""
+        if(this.state.homeMounted){
+            homeCmp = (
+                        <Home 
+                            name={"Max"} 
+                            age={27} 
+                            greet={this.onGreet}
+                            changeLink = {this.onChangeLinkName.bind(this)}
+                            initialLinkName = {this.state.homeLink}
+                        />
+                    );
+        }
         return (
             <div className="container">
                 <div className="row">
@@ -31,14 +52,12 @@ class App extends React.Component {
                 <br/>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home 
-                            name={"Max"} 
-                            age={27} 
-                            greet={this.onGreet}
-                            changeLink = {this.onChangeLinkName.bind(this)}
-                            initialLinkName = {this.state.homeLink}
-                        />
-                        
+                        {homeCmp}                        
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <button className="btn btn-primary" onClick={this.onChangeHomeMounted.bind(this)}>(Un)Mount Home Component</button>                      
                     </div>
                 </div>
             </div>
